c793da95f89869f9c5dc751bbc8b3963549b98f6
hunktuck <ddikshakk@gmail.com>
2024-06-07 11:57:57 +0530

    s3 setup done

--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,12 @@ const express= require('express')
 
 const postRouter= express.Router()
 
+postRouter.post('/', async(req,res)=> {
+    try{
+        const data= req.body
+    }catch(err){
+        res.json(err)
+    }
+})
+
 module.exports= postRouter
